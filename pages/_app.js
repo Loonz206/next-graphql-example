6a553a5b2550c656ee27128de76aa97cc90404e1
Lenny Peters <mr.lennypeters@gmail.com>
2020-12-22 19:50:20 -0800

    refactor: 💡 initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import React from "react";
+import "../src/styles/globals.scss";
 
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
